1f81f79ffc682f1c218ea26b57b14969a1c8a176
Jakub Michalski <130910084+jakub1m@users.noreply.github.com>
2024-12-11 18:31:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -48,14 +48,14 @@ URL_ADMINPAGE = "http://127.0.0.1"
 
 # File Paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-AUDIO_FOLDER_PATH = r"E:\AIMP_20_11_2024\aimp_cursor\audio"
+AUDIO_FOLDER_PATH = os.path.join(BASE_DIR, "audio")
 AUDIO_FOLDER_TEMP_PATH = r"E:\AIMP_20_11_2024\aimp_cursor\audio_temp"
 AIMP_PLAYLIST_PATH = os.path.join(os.environ['USERPROFILE'], 'AppData', 'Roaming', 'AIMP', 'PLS')
 PLAYED_SONGS_FILE = os.path.join(BASE_DIR, "played_songs.txt")
 BLACKLISTED_SONGS = os.path.join(BASE_DIR, "blacklisted_songs.txt")
 PROMPT_SENTIMENT = os.path.join(BASE_DIR, "prompts", "sentiment_prompt.txt")
 PROMPT_TRANSCRIPTION = os.path.join(BASE_DIR, "prompts", "transcription_prompt.txt")
-PRIORITY_PLAYLISTS_DIR = os.path.join(BASE_DIR, "priority_playlists")
+SPECIAL_PLAYLISTS_PATH = os.path.join(BASE_DIR, "special_playlists")
 
 # Audio Device Settings
 AUDIO_DEVICE_NAME = "HDTV" # korytarz "Miks Stereo"
@@ -64,6 +64,9 @@ AIMP_VOLUME_INCREMENT = 750
 AIMP_MAX_VOLUME = 65535
 
 # Schedule Times
-PLAYLIST_UPDATE_TIMES = ["07:45","08:40", "09:35", "10:30", "11:25", "12:25", "13:20", "14:15","15:10"]
-DEVICE_START_TIMES = ["07:51","08:46", "09:41", "10:36", "11:31", "12:31", "13:26", "14:21","15:16"]
-DEVICE_STOP_TIMES = ["07:59", "08:54", "09:49", "10:44", "11:44", "12:39", "13:34", "14:29","15:24"]
+PLAYLIST_UPDATE_TIMES = ["07:45","08:40", "09:35", "10:30", "11:25", "12:25", "13:20", "14:15","15:10", "13:45"]
+DEVICE_START_TIMES = ["07:51","08:46", "09:41", "10:36", "11:31", "12:31", "13:26", "14:21","15:16","21:37"]
+DEVICE_STOP_TIMES = ["07:59", "08:54", "09:49", "10:44", "11:44", "12:39", "13:34", "14:29","15:24","21:39"]
+
+SCHOOL_BELLS_START = ["07:50","08:45", "09:40", "10:35", "11:30", "12:30", "13:25", "14:20","15:15"]
+SCHOOL_BELLS_END = ["08:00", "08:55", "09:50", "10:45", "11:45", "12:40", "13:35", "14:30"]
